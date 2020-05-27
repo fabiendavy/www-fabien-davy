35459451805e8aa635d8cf0cbfb658019ce3af13
Fabien Davy <davyfabien@gmail.com>
2020-05-27 18:20:24 +0200

    clean html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,13 +1,11 @@
 import '../styles/application.scss';
 import { initTypedJs } from './init_typed';
-import { fadeInScroll } from './fadeInScroll';
 import { navbar, displayNavbar } from './navbar';
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 
-window.addEventListener('load', () => {
+window.addEventListener('DOMContentLoaded', () => {
   initTypedJs();
-  // fadeInScroll();
   navbar();
   displayNavbar();
   AOS.init();
